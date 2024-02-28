2fc483bbe609854880177a9a1bdcf522d6c24089
j4n377 <anjanettesumalo@gmail.com>
2024-02-28 15:30:02 +0800

    exercise_4.rb

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,3 +2,14 @@
 #  Make a Hash of names with unique values age.
 #  Write a function that takes an age and returns the name.
 #  Print on console the result.
+
+def find_name (age_value)
+  name={ann:18,jasmine:21,alex:43,jude:5}
+  name.each do |name,age|
+    if age == age_value
+      return name
+    end
+  end
+end
+
+p find_name(5)
