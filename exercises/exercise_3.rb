5b761083016a129203da1c186f4c8c595a5c76ac
j4n377 <anjanettesumalo@gmail.com>
2024-02-28 15:18:53 +0800

    exercise_3.rb

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -3,8 +3,15 @@
 #  the function will return an array of ages that is considered adult.
 #  Print on console the result.
 
-def adult? (ages)
-  adult = []
-  [age] each.do |age|
-
+def who_is_an_adult?(ages)
+  adults=[]
+  ages.each do |age|
+    if age>=18
+      adults << age
+    end
+  end
+  return adults
 end
+
+p who_is_an_adult?([2,18,90,15])
+p who_is_an_adult?([90,10,5,5])
